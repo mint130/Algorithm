d54f0889ba4194e8ee9e811d436c8c875078c767
mint130 <110026510+mint130@users.noreply.github.com>
2023-11-29 17:06:40 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -8,16 +8,6 @@ vector <int> n;
 //vector <int> m;
 
 
-bool binary_search(vector<int>& arr, int low, int high, int target) {
-	
-	if (low > high) return false;
-	int mid = (high + low) / 2;
-	if (arr[mid] == target) return true;
-	if (arr[mid] < target) // high = mid - 1;
-		return binary_search(arr, mid + 1, high, target); 
-	else //arr[mid]>target
-		return binary_search(arr, low, mid - 1, target);
-}
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
@@ -34,7 +24,7 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		int k;
 		cin >> k;
-		cout<<binary_search(n, 0, N-1, k)<<"\n";
+		cout<<binary_search(n.begin(), n.end(), k)<<"\n";
 	}
 
 }
