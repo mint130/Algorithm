f3e3cb257951a2dd8b91dfe991c1527b06fb9054
mint130 <110026510+mint130@users.noreply.github.com>
2023-11-29 16:03:33 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	priority_queue<int, vector<int>, greater<int>> pq;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (num == 0) {
+			//0인 경우 큰 값 출력하고 제거
+			if (pq.empty()) {
+				cout << 0 << "\n";
+			}
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(num);
+		}
+		
+	}
+}
