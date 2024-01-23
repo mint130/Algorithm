947b113f9a52715b42cf901a719989dc5c476c90
mint130 <110026510+mint130@users.noreply.github.com>
2024-01-24 01:07:05 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 5292 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -4,17 +4,67 @@ using namespace std;
 
 int n;
 int arr[1000002];
-priority_queue<int, vector<int>, greater<int>> pq;
+int tmp[1000002];
+void merge(int st, int en) {
+	int mid = (st + en) / 2;
+
+	int idx_l = st;
+	int idx_r = mid;
+	int idx_t = st;
+
+	while (idx_l < mid && idx_r < en) {
+		if (arr[idx_l] < arr[idx_r]) {
+			tmp[idx_t] = arr[idx_l];
+			idx_l++;
+			idx_t++;
+		}
+		else if (arr[idx_l] >= arr[idx_r]) {
+			tmp[idx_t] = arr[idx_r];
+			idx_r++;
+			idx_t++;
+		}
+	}
+	while (idx_l < mid) {
+		tmp[idx_t] = arr[idx_l];
+		idx_t++;
+		idx_l++;
+	}
+	while (idx_r < en) {
+		tmp[idx_t] = arr[idx_r];
+		idx_t++;
+		idx_r++;
+	}
+	for (int i = st; i < en; i++) {
+		arr[i] = tmp[i];
+	}
+}
+//priority_queue<int, vector<int>, greater<int>> pq;
+void merge_sort(int st, int en) {
+	if (en == st + 1) {
+		return;
+	}
+	int mid = (st + en) / 2;
+	merge_sort(st, mid);
+	merge_sort(mid, en);
+	merge(st, en);
+}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
+	}
+	/*for (int i = 0; i < n; i++) {
+		cin >> arr[i];
 		pq.push(arr[i]);
 	}
 	while (!pq.empty()) {
 		cout<<pq.top()<<"\n";
 		pq.pop();
+	}*/
+	merge_sort(0, n);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << "\n";
 	}
 }
