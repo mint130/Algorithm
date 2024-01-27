18076d06cd73635d711a4cfc80183663c192962b
mint130 <110026510+mint130@users.noreply.github.com>
2024-01-27 23:20:30 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+
+int arr[10];
+int ans[10];
+
+
+void func(int k, int idx) {
+	
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	int tmp = 0; //k번째 항을 채울 때마다 초기화
+	for (int i = idx; i < n; i++) {
+		//이전 수열의 마지막 항과 새로운 수열의 마지막 항이 같으면 중복 수열
+		//arr의 i번째 값이 tmp와 같지 않을 때
+		if (arr[i] != tmp) {
+			ans[k] = arr[i];
+			tmp = ans[k]; //tmp는 이전에 선택한 값=이전 수열의 마지막 값
+			func(k + 1, i);
+
+		}
+
+		//중복되는 수열을 여러번 출력하면 안 됨
+	}
+	
+
+	
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		
+	}
+	
+	sort(arr, arr + n);
+	
+	
+
+	func(0, 0);
+	//k개가 채워진 수열
+}
