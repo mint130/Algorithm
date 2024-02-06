dbb1a2ff99f9632fdc3d367e6dd12259ec2cba86
mint130 <110026510+mint130@users.noreply.github.com>
2024-02-06 16:35:36 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
-
 using namespace std;
-vector <int> n;
-//vector <int> m;
-
-
+int n, m;
+int arr[100002];
+int binarysearch(int target, int start, int finish) {
+	if (start > finish) return 0;
+	int mid = (start + finish) / 2;
+	if (arr[mid] == target) return 1;
+	if (arr[mid] < target) {
+		return binarysearch(target, mid + 1, finish);
+	}
+	else {
+		return binarysearch(target, start, mid - 1);
+	}
+}
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	int N, M;
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int k;
-		cin >> k;
-		n.push_back(k);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
 	}
-	sort(n.begin(), n.end());
-	cin >> M;
-	for (int i = 0; i < M; i++) {
-		int k;
-		cin >> k;
-		cout<<binary_search(n.begin(), n.end(), k)<<"\n";
+	sort(arr, arr + n);
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		cout << binarysearch(num, 0, n-1) << "\n";
 	}
-
 }
