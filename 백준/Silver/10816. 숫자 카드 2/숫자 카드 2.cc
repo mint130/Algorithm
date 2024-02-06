acaa13124809113d5eaebadd3d4877b2f44e126e
mint130 <110026510+mint130@users.noreply.github.com>
2024-02-06 18:44:33 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 316 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -7,37 +7,7 @@ using namespace std;
 int n, m;
 int arr[500002];
 //target이 들어갈 수 있는 가장 왼쪽 위치
-int lower_idx(int target, int len) {
-	int start = 0;
-	int end = len;
-	while (start != end) {
-		int mid = (start + end) / 2;
-		if (arr[mid] >= target) {
-			end = mid;
-		}
-		else {
-			start = mid + 1;
-		}
-	}
-	return start;
-}
-//target이 들어갈 수 있는 가장 오른쪽 위치
-int upper_idx(int target, int len) {
-	int start = 0;
-	int end = len;
-	while (start != end) {
-		int mid = (start + end) / 2;
-		if (arr[mid] > target) {
-			//target이 내가 찾은 위치보다 작아야 함
-			end = mid;
-		}
-		else {
-			start = mid + 1;
-		}
-	}
-	return start;
 
-}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -50,7 +20,8 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int num;
 		cin >> num;
-		cout << upper_idx(num, n) - lower_idx(num, n) << " ";
+		//cout << upper_idx(num, n) - lower_idx(num, n) << " ";
+		cout << upper_bound(arr, arr + n, num) - lower_bound(arr, arr + n, num) << " ";
 	}
 	
 }
