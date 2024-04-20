f57af9e6ab4257dcb4491367ec2c89f252a64c26
mint130 <110026510+mint130@users.noreply.github.com>
2024-04-20 23:13:40 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> node[202];
+queue<int> q;
+bool visited[202];
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) continue;
+            if(computers[i][j]){
+                node[i].push_back(j);
+                //node[j].push_back(i);
+            }
+        }
+    }
+
+    
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            q.push(i);
+            visited[i]=1;
+            answer++;
+            while(!q.empty()){
+                int cur=q.front();
+                q.pop();
+
+                for(auto nxt:node[cur]){
+                    if(!visited[nxt]){  
+                        q.push(nxt);
+                        visited[nxt]=1;
+                    }
+                }
+            }
+            
+        }
+        //for(auto j:node[i]){
+        //    cout<<i<<" "<<j<<endl;
+        //}
+    }
+    
+    
+    return answer;
+}
