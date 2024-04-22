c29509f7c7b057bcaac52745d26cf891b98f8650
mint130 <110026510+mint130@users.noreply.github.com>
2024-04-22 21:29:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> v;
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) v.push_back(i);
+	}
+	if (v.size() < k) cout << 0;
+	else {
+		cout << v[k - 1];
+	}
+	
+}
