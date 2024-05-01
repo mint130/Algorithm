85ddd957cb61b887c43da8e6c1431bf013955856
mint130 <110026510+mint130@users.noreply.github.com>
2024-05-02 01:49:03 +0900

    [level 2] Title: 숫자 변환하기, Time: 2.22 ms, Memory: 7.43 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+int dist[1000002];
+queue<int> q;
+int solution(int x, int y, int n) {
+    //int answer = -1;
+    fill(dist, dist+1000002, -1);
+    q.push(x);
+    dist[x]=0;
+    bool flag=0;
+    while(!q.empty()){
+        int cur=q.front();
+        q.pop();
+        if(cur==y){
+            break;
+        }
+        //cur+n
+        if(cur+n<=y && dist[cur+n]==-1){
+            dist[cur+n]=dist[cur]+1;
+            q.push(cur+n);
+        }
+        //cur*2
+        if(cur*2<=y && dist[cur*2]==-1){
+            dist[cur*2]=dist[cur]+1;
+            q.push(cur*2);
+        }
+        //cur*3
+        if(cur*3<=y){
+            dist[cur*3]=dist[cur]+1;
+            q.push(cur*3);
+        }
+    }
+    return dist[y];
+}
