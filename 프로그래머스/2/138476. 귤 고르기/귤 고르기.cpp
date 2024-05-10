c3751da2da4ba10575025cc24f17daa805f4eaf4
mint130 <110026510+mint130@users.noreply.github.com>
2024-05-10 20:23:45 +0900

    [level 2] Title: 귤 고르기, Time: 23.67 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -10,16 +10,13 @@ int solution(int k, vector<int> tangerine) {
     int answer = 0;
     //서로 다른 종류의 원소 최소값
     //k는 한 종류에 담으려는 귤의 갯수
-    //vector<int> cnt[10000001];
     for(int i=0;i<tangerine.size();i++){
         int size=tangerine[i];
-        //cnt[size]++;
         m[size]++;
     }
-    priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
+    priority_queue<int> pq;
     for(auto i:m){
-        //cout<<i.first<<" "<<i.second<<endl;
-        pq.push({i.second, i.first});
+        pq.push(i.second);
     }
     int cnt=0; //종류
     while(!pq.empty()){
@@ -27,7 +24,7 @@ int solution(int k, vector<int> tangerine) {
             answer=cnt;
             break;
         }
-        k=k-pq.top().first; //갯수
+        k=k-pq.top(); //갯수
         cnt++;
         pq.pop();
     }
