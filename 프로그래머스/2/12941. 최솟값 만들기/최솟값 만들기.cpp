9e38f9fb7e09f0cb3c1732ae9df66e2de2e20832
mint130 <110026510+mint130@users.noreply.github.com>
2024-05-21 02:11:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.07 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -8,14 +8,13 @@ int solution(vector<int> A, vector<int> B)
     int n=A.size();
     int answer = 0;
     sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
+    sort(B.rbegin(), B.rend());
    
-    int sum1=0; int sum2=0;
+    
     for(int i=0;i<n;i++){
-        sum1+=A[i]*B[n-1-i];
-        sum2+=B[i]*A[n-1-i];
+        answer+=A[i]*B[i];
     }
-    answer=min(sum1, sum2);
+    
 
     
     
