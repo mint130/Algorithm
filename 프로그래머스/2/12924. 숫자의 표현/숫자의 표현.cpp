e5174ba6d1414e1d500979febd73fed1dec765d8
mint130 <110026510+mint130@users.noreply.github.com>
2024-05-21 13:02:18 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -5,22 +5,16 @@ using namespace std;
 int arr[10001];
 int solution(int n) {
     int answer = 0;
-    for(int i=1;i<=n;i++){
-        arr[i]=i;
-    }
-    int en=1;int st=1;
-    int sum=arr[1];
-    while(st<=en && en<=n){
-        if(sum<=n){
-            if(sum==n) answer++;
-            en++;
-            sum+=arr[en];
-        }
-        else if(sum>n){
-            sum-=arr[st];
-            st++;
-        }
     
+    for(int i=1;i<=n-1;i++){
+        int sum=i;
+        for(int j=i+1;j<=n-1;j++){
+            sum+=j;
+            if(sum==n){
+                answer++;
+            }
+            else if(sum>n) break;
+        }
     }
-    return answer;
+    return answer+1;
 }
