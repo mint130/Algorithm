664a2943d6c2e86b3d3c4f8097ef78d7d688384e
mint130 <110026510+mint130@users.noreply.github.com>
2024-06-01 23:03:18 +0900

    [level 2] Title: 모음 사전, Time: 0.21 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+string w[5]={"A", "E", "I", "O", "U"};
+vector<string> s;
+void dfs(string str, int cnt){
+    s.push_back(str);
+    //cnt가 5가 되면 종료
+    if(cnt==5){
+        return;
+    }
+    for(int i=0;i<5;i++){
+        dfs(str+w[i], cnt+1);
+    }
+    
+}
+int solution(string word) {
+    for(int i=0;i<5;i++){
+        dfs(w[i], 1);
+    }
+    return find(s.begin(), s.end(), word)-s.begin()+1;
+    
+}
