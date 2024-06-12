1f19a30e96d3b73c6724cdd86293b7b7be5f45c8
mint130 <110026510+mint130@users.noreply.github.com>
2024-06-12 17:17:47 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+    int n=want.size();
+    //discount는 10개씩 끊어서
+    int idx=0;
+    vector<int> num(number.begin(), number.end());
+    //m1은 초기값
+    while(idx+10<=discount.size()){
+        //idx부터 10개씩 want에 있는거 확인하고 want에 1이라도 남아 있으면 idx++, 원래대로 want 복구
+        for(int i=0;i<10;i++){
+            string s=discount[idx+i];
+            for(int j=0;j<n;j++){
+                if(want[j]==s){
+                    num[j]--;
+                }
+            }
+        }
+        bool flag=0;
+        for(int j=0;j<n;j++){
+            if(num[j]!=0) flag=1;
+            num[j]=number[j]; //복구
+        }
+        if(!flag) answer++;
+        idx++;
+    }
+    return answer;
+}
