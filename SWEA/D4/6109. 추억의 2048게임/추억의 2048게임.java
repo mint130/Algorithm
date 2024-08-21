3a0c4e93dfd19bf58a2452c69185d00778a07ef2
mint130 <110026510+mint130@users.noreply.github.com>
2024-08-22 00:36:47 +0900

    [D4] Title: 추억의 2048게임, Time: 260 ms, Memory: 46,444 KB -BaekjoonHub

--- SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
+++ SWEA/D4/6109. 추억의 2048게임/추억의 2048게임.java
@@ -11,118 +11,55 @@ public class Solution {
 	static int T, N;
 	static int dir;
 	static int board[][];
-	static void move(String S) {
-		if(S.equals("left")) {
-			for(int i=0;i<N;i++) {
-				Queue<Integer> q = new LinkedList<>();
-				for(int j=0;j<N;j++) {
-					if(board[i][j]!=0) q.add(board[i][j]);
+	static int temp[][];
+	static void move(int a, int b) {
+		//0 0 left 
+		//0 1 right
+		//1 0 up
+		//1 1 down
+		
+		//a가 0이면 board[i][j]
+		//a가 1이면 board[j][i]
+		//b가 0이면 0부터 N-1
+		//b가 1이면 N-1부터 0
+		
+		for(int i=0;i<N;i++) {
+			Queue<Integer> q = new LinkedList<>();
+			int ni=0;
+			int nj=0;
+			for(int j=0;j<N;j++) {
+				if(a==0 && b==0) {ni=i; nj=j;}
+				if(a==1 && b==0) {ni=j; nj=i;}
+				if(a==0 && b==1) {ni=i; nj=N-1-j;}
+				if(a==1 && b==1) {ni=N-1-j; nj=i;}
+				if(board[ni][nj]!=0) q.add(board[ni][nj]);
+			}
+			int idx = 0;
+			while(!q.isEmpty()) {
+				int top = q.poll();
+				if(!q.isEmpty() && top==q.peek()) {
+					top*=2;
+					q.poll();
 				}
-				int idx = 0;
-				while(!q.isEmpty()) {
-					int top=q.poll(); //가장 위의 값
-					if(q.size()>=1) {
-						if(top==q.peek()) {
-							board[i][idx]=top*2;
-							q.poll();
-						}
-						else {
-							board[i][idx]=top;
-						}
-					}
-					else board[i][idx]=top;
-					idx++;
-				}
-				while(idx<N) {
-					board[i][idx]=0;
-					idx++;
-				}
+				if(a==0 && b==0) {ni=i; nj=idx;}
+				if(a==1 && b==0) {ni=idx; nj=i;}
+				if(a==0 && b==1) {ni=i; nj=N-1-idx;}
+				if(a==1 && b==1) {ni=N-1-idx; nj=i;}
+				board[ni][nj]=top;
+				idx++;
+			}
+			while(idx<N) {
+				if(a==0 && b==0) {ni=i; nj=idx;}
+				if(a==1 && b==0) {ni=idx; nj=i;}
+				if(a==0 && b==1) {ni=i; nj=N-1-idx;}
+				if(a==1 && b==1) {ni=N-1-idx; nj=i;}
+				board[ni][nj]=0;
+				idx++;
 			}
 		}
-		else if(S.equals("right")) {
-			for(int i=0;i<N;i++) {
-				Queue<Integer> q = new LinkedList<>();
-				for(int j=N-1;j>=0;j--) {
-					if(board[i][j]!=0) q.add(board[i][j]);
-				}
-				int idx = 0;
-				while(!q.isEmpty()) {
-					int top=q.poll(); //가장 위의 값
-					if(q.size()>=1) {
-						if(top==q.peek()) {
-							board[i][N-1-idx]=top*2;
-							q.poll();
-						}
-						else {
-							board[i][N-1-idx]=top;
-						}
-					}
-					else board[i][N-1-idx]=top;
-					idx++;
-				}
-				while(idx<N) {
-					board[i][N-1-idx]=0;
-					idx++;
-				}
-			}
-			
-		}
-		else if(S.equals("up")) {
-			for(int i=0;i<N;i++) {
-				Queue<Integer> q = new LinkedList<>();
-				for(int j=0;j<N;j++) {
-					if(board[j][i]!=0) q.add(board[j][i]);
-				}
-				int idx = 0;
-				while(!q.isEmpty()) {
-					int top=q.poll(); //가장 위의 값
-					if(q.size()>=1) {
-						if(top==q.peek()) {
-							board[idx][i]=top*2;
-							q.poll();
-						}
-						else {
-							board[idx][i]=top;
-						}
-					}
-					else board[idx][i]=top;
-					idx++;
-				}
-				while(idx<N) {
-					board[idx][i]=0;
-					idx++;
-				}
-			}
-		}
-		else {
-			for(int i=0;i<N;i++) {
-				Queue<Integer> q = new LinkedList<>();
-				for(int j=N-1;j>=0;j--) {
-					if(board[j][i]!=0) q.add(board[j][i]);
-				}
-				int idx = 0;
-				while(!q.isEmpty()) {
-					int top=q.poll(); //가장 위의 값
-					if(q.size()>=1) {
-						if(top==q.peek()) {
-							board[N-1-idx][i]=top*2;
-							q.poll();
-						}
-						else {
-							board[N-1-idx][i]=top;
-						}
-					}
-					else board[N-1-idx][i]=top;
-					idx++;
-				}
-				while(idx<N) {
-					board[N-1-idx][i]=0;
-					idx++;
-				}
-			}
-			
-		}
+	
 	}
+
 	static void print() {
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<N;j++) {
@@ -143,13 +80,29 @@ public class Solution {
 			String S = st.nextToken();
 
 			board=new int[N][N];
+			temp=new int[N][N];
 			for(int i=0;i<N;i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j=0;j<N;j++) {
 					board[i][j]=Integer.parseInt(st.nextToken());
+					temp[i][j]=board[i][j];
 				}
 			}
-			move(S);
+			switch(S) {
+			case "left":
+				move(0, 0);
+				break;
+			case "right":
+				move(0, 1);
+				break;
+			case "up":
+				move(1, 0);
+				break;
+			case "down":
+				move(1, 1);
+				break;
+			}
+			
 			System.out.println("#"+tc);
 			print();
 		}
