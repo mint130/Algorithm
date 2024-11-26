fcfeb9e7ce0de33bf778e3cfcda39d517eab8127
mint130 <110026510+mint130@users.noreply.github.com>
2024-11-26 13:28:21 +0900

    [Gold V] Title: 감시 피하기, Time: 108 ms, Memory: 15320 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -1,104 +1,90 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static List<int[]> student;
-	static boolean flag;
-	static char board[][];
-	static int dx[]= {1, 0, -1, 0};
-	static int dy[]= {0, 1, 0, -1};
-	static boolean isOk() {
-		//감시 피할수있는지 
-		for(int cur[]:student) {
-			int x = cur[0];
-			int y = cur[1];
-			//상 
-			//하
-			//좌
-			//우 
-			boolean o=false;
-			for(int i=x;i>=0;i--) {
-				//상 
-				if(board[i][y]=='O') o=true;
-				if(board[i][y]=='T' && !o) return false;
-			}
-			o=false;
-			for(int i=x;i<N;i++) {
-				//하 
-				if(board[i][y]=='O') o=true;
-				if(board[i][y]=='T' && !o) return false;
-			}
-			o=false;
-			for(int i=y;i>=0;i--) {
-				if(board[x][i]=='O') o=true;
-				if(board[x][i]=='T' && !o) return false;
-			}
-			o=false;
-			for(int i=y;i<N;i++) {
-				if(board[x][i]=='O') o=true;
-				if(board[x][i]=='T' && !o) return false;
+	private static int N;
+	private static char[][] Map;
+	private static ArrayList<int[]> TeacherPositions;
+	private static final int[] ROW_DIRS = { -1, 1, 0, 0 };
+	private static final int[] COL_DIRS = { 0, 0, -1, 1 };
+	private static boolean isFindAnswer;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		Map = new char[N][N];
+		TeacherPositions = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			for (int j = 0; j < N; j++) {
+				Map[i][j] = st.nextToken().charAt(0);
+
+				// 선생 위치 저장
+				if (Map[i][j] == 'T')
+					TeacherPositions.add(new int[] { i, j });
 			}
 		}
-		return true;
-	}
-	static void dfs(int cnt) {
-		if(cnt==3) {
-			if(flag) {
-				//이미 피할수있음 
-				return;
-			}
-			else {
-				//학생들을 감시로부터 피하도록 할 수 있음 
-				if(isOk()) {
-					flag=true;
-					return;
-				}
-			}	
-			
-			return;
-		
-		}
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(board[i][j]=='X') {
-					//빈칸에 장애물 넣음 
-					board[i][j]='O';
-					dfs(cnt+1);
-					board[i][j]='X';
-				}
-				
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st =new StringTokenizer(br.readLine());
-		N=Integer.parseInt(st.nextToken());
-		board =new char[N][N];
-		student = new ArrayList<>();
-		for(int i=0;i<N;i++) {
-			st=new StringTokenizer(br.readLine());
-			for(int j=0;j<N;j++) {
-				String s = st.nextToken();
-				board[i][j]=s.charAt(0);
-				if(board[i][j]=='S') {
-					student.add(new int[] {i,j});
-				}
-			}
-		}
-		
-		dfs(0);
-		if(flag) System.out.println("YES");
-		else System.out.println("NO");
-		
+
+		// 2차원 조합
+		combination(0, 0);
+		String answer = (isFindAnswer) ? "YES" : "NO";
+
+		System.out.println(answer);
+		br.close();
 	}
 
+	// 1차원 배열로 변환한 2차원 조합 방법
+	private static void combination(int index, int count) {
+		if (isFindAnswer)
+			return;
+
+		if (count == 3) {
+			if (isAllPass())	isFindAnswer = true;
+			return;
+		}
+
+		// 더 이상 설치할 공간이 없는 경우
+		if (index >= N * N)
+			return;
+
+		int row = index / N;
+		int col = index % N;
+
+		// 현재 위치가 빈 칸일 경우
+		if (Map[row][col] == 'X') {
+			Map[row][col] = 'O';
+			combination(index + 1, count + 1);
+			Map[row][col] = 'X';
+		}
+
+		// 현재 위치에 장애물을 설치하지 않는 경우
+		combination(index + 1, count);
+	}
+
+	private static boolean isAllPass() {
+		for (int[] position : TeacherPositions) {
+			for (int i = 0; i < 4; i++) {
+				int row = position[0] + ROW_DIRS[i], col = position[1] + COL_DIRS[i];
+
+				while (isInMap(row, col) && Map[row][col] != 'O') {
+					if (Map[row][col] == 'S')
+						return false;
+					row += ROW_DIRS[i];
+					col += COL_DIRS[i];
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private static boolean isInMap(int row, int col) {
+		return row >= 0 && row < N && col >= 0 && col < N;
+	}
 }
