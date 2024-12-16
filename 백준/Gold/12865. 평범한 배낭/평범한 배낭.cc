ec48c97b3096839741143950432582b15b7e8d54
mint130 <110026510+mint130@users.noreply.github.com>
2024-12-17 03:52:45 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,42 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
-vector<pair<int, int>> a;
-int n, k;
-int dp[102][100002]; //배낭에 넣는 물건 번호/무게
-int w[102];
-int v[102];
+int n, k; //물건 n, 무게 k, 무게는 최대 k
+int value[101]; 
+int weight[101];
+int dp[101][100002]; //dp[i][k] 최대 k무게까지 담을 수 있고 i까지 고려했을 때의 최대 가치
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> w[i] >> v[i];
-	}
-	//베낭 용량이 0
-	for (int i = 0; i <= n; i++) {
-		dp[i][0] = 0;
-	}
-	//어떤 물건도 담지 않음
-	for (int i = 0; i <= k; i++) {
-		dp[0][i] = 0;
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			//물건 i를 베낭에 담지 않는 경우 dp[i][j]=dp[i-1][j]
-			//물건 i를 베낭에 담는 경우 현재 용량이 j-w[i]인 경우의 최대 가치 + v[i] 
-			//j는 베낭 무게
-			if (j < w[i]) {
-				//베낭보다 현재 물건 i의 무게가 더 크면 담지 않음
-				dp[i][j] = dp[i - 1][j];
-			}
-			else {
-				//현재 물건 i를 담지 않거나 담거나
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-			}
-		}
-	}
-	cout << dp[n][k];
-	
+    cin>>n>>k;
+    for(int i=1;i<=n;i++){
+        int w, v;
+        cin>>w>>v;
+        value[i]=v;
+        weight[i]=w;
+    }
+    for(int i=0;i<=k;i++){
+        //초기화: 어떤 물건도 담지 않음
+        dp[0][i]=0;
+    }
+    for(int i=0;i<=n;i++){
+        //초기화: 베낭의 무게 0 
+        dp[i][0]=0;
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=k;j++){
+            //현재 물건의 번호 i, 가방의 무게 k
+            if(j<weight[i]){
+                //현재 물건의 무게가 가방 무게 k보다 크면 못담는다
+                dp[i][j]=dp[i-1][j];
+            }
+            else {
+                //현재 i의 물건을 담거나 담지 않거나 중 더 가치가 큰 것
+                dp[i][j]=max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]);
+            }
+        }
+    }
+
+    cout<<dp[n][k];
 }
