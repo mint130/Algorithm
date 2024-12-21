bf8a1556fc9849c77d608c605fd4a4095b5c2a19
김민주 <110026510+mint130@users.noreply.github.com>
2024-12-21 22:17:16 +0900

    [Gold V] Title: 배열 탈출, Time: 1184 ms, Memory: 40628 KB -BaekjoonHub

--- 백준/Gold/11909. 배열 탈출/배열 탈출.cc
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.cc
@@ -6,52 +6,54 @@ using namespace std;
 
 int n;
 int board[2223][2223];
-int dp[2223][2223]; //board[i][j]까지 왔을 때 최소비용
-int main() {
-    cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>board[i][j];
-            dp[i][j]=987654321;
+int dp[2223][2223]; // board[i][j]까지 왔을 때 최소비용
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> board[i][j];
+            dp[i][j] = 987654321;
         }
     }
-    dp[0][0]=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 && j==0) continue;
+    dp[0][0] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 && j == 0)
+                continue;
             int cur = board[i][j];
-            int left =0;
+            int left = 0;
             int top = 0;
-            if(i==0){
-                left = board[i][j-1];
-                //현재가 더 큼
-                if(left<=cur){
-                    dp[i][j]=min(dp[i][j], dp[i][j-1]+cur-left+1);
-                }
-                else dp[i][j]=min(dp[i][j], dp[i][j-1]);
-            }
-            else if(j==0){
-                top = board[i-1][j];
-                if(top<=cur){
-                    dp[i][j]=min(dp[i][j], dp[i-1][j]+cur-top+1);
-                }
-                else dp[i][j]=min(dp[i][j], dp[i-1][j]);
-            }
-            else {
-                left = board[i][j-1]; //왼쪽
-                top = board[i-1][j]; //위쪽
 
-                if(top<=cur){
-                    dp[i][j]=min(dp[i][j], dp[i-1][j]+cur-top+1);   
+            if (j != 0)
+                left = board[i][j - 1]; // 왼쪽
+            if (i != 0)
+                top = board[i - 1][j]; // 위쪽
+
+            if (i != 0)
+            {
+                if (top <= cur)
+                {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + cur - top + 1);
                 }
-                else dp[i][j]=min(dp[i][j], dp[i-1][j]);
-                if(left<=cur){
-                    dp[i][j]=min(dp[i][j], dp[i][j-1]+cur-left+1);
+                else
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+            }
+            if (j != 0)
+            {
+                if (left <= cur)
+                {
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + cur - left + 1);
                 }
-                else dp[i][j]=min(dp[i][j], dp[i][j-1]);
+                else
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1]);
             }
         }
     }
-   
-    cout<<dp[n-1][n-1];
+
+    cout << dp[n - 1][n - 1];
 }
