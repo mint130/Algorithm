e216d3cb422eaca43049ce008ad0d861c027cf42
김민주 <110026510+mint130@users.noreply.github.com>
2025-01-02 23:18:35 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,5 @@
+select ins.name as name, ins.datetime as datetime
+from animal_ins ins left join animal_outs outs on ins.animal_id =outs.animal_id
+where outs.animal_id is null
+order by ins.datetime
+limit 3
