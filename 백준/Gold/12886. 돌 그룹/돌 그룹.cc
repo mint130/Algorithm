981be7b4cd200ea1a10aad09bb2d7b843ec18ffe
김민주 <110026510+mint130@users.noreply.github.com>
2025-01-09 17:23:30 +0900

    [Gold IV] Title: 돌 그룹, Time: 28 ms, Memory: 2668 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.cc
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -6,7 +6,7 @@
 using namespace std;
 
 int A, B, C;
-bool visit[500][500][500];
+bool visit[500][500]; //min과 max만
 
 int main() {
 	cin >> A >> B >> C;
@@ -15,13 +15,12 @@ int main() {
 	sort(v.begin(), v.end());
 	queue<vector<int>> q;
 	q.push(v);
-	visit[v[0]][v[1]][v[2]] = 1;
+	visit[v[0]][v[2]] = 1;
 	while (!q.empty()) {
 		vector<int> cur = q.front();
 		int a = cur[0];
 		int b = cur[1];
 		int c = cur[2];
-
 		q.pop();
 		if (a == b && b == c) {
 			cout << 1;
@@ -36,8 +35,8 @@ int main() {
 			nxt.push_back(y);
 			nxt.push_back(z);
 			sort(nxt.begin(), nxt.end());
-			if (!visit[nxt[0]][nxt[1]][nxt[2]]) {
-				visit[nxt[0]][nxt[1]][nxt[2]] = 1;
+			if (!visit[nxt[0]][nxt[2]]) {
+				visit[nxt[0]][nxt[2]] = 1;
 				q.push(nxt);
 			}
 		}
@@ -50,8 +49,8 @@ int main() {
 			nxt.push_back(y);
 			nxt.push_back(z);
 			sort(nxt.begin(), nxt.end());
-			if (!visit[nxt[0]][nxt[1]][nxt[2]]) {
-				visit[nxt[0]][nxt[1]][nxt[2]] = 1;
+			if (!visit[nxt[0]][nxt[2]]) {
+				visit[nxt[0]][nxt[2]] = 1;
 				q.push(nxt);
 			}
 		}
@@ -59,5 +58,5 @@ int main() {
 	}
 	cout << 0;
 	return 0;
-	
+
 }
