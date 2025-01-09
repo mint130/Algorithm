4b5af93756e96f86b47eb75f55ff22a0af34b38f
김민주 <110026510+mint130@users.noreply.github.com>
2025-01-09 16:33:51 +0900

    [Gold IV] Title: 돌 그룹, Time: 84 ms, Memory: 124496 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int A, B, C;
+bool visit[500][500][500];
+
+int main() {
+	cin >> A >> B >> C;
+	vector<int> v;
+	v.push_back(A); v.push_back(B); v.push_back(C);
+	sort(v.begin(), v.end());
+	queue<vector<int>> q;
+	q.push(v);
+	visit[v[0]][v[1]][v[2]] = 1;
+	while (!q.empty()) {
+		vector<int> cur = q.front();
+		int a = cur[0];
+		int b = cur[1];
+		int c = cur[2];
+
+		q.pop();
+		if (a == b && b == c) {
+			cout << 1;
+			return 0;
+		}
+		if (b - a >= 0) {
+			int x = a * 2;
+			int y = b - a;
+			int z = c;
+			vector<int> nxt;
+			nxt.push_back(x);
+			nxt.push_back(y);
+			nxt.push_back(z);
+			sort(nxt.begin(), nxt.end());
+			if (!visit[nxt[0]][nxt[1]][nxt[2]]) {
+				visit[nxt[0]][nxt[1]][nxt[2]] = 1;
+				q.push(nxt);
+			}
+		}
+		if (c - a >= 0) {
+			int x = a * 2;
+			int y = c - a;
+			int z = b;
+			vector<int> nxt;
+			nxt.push_back(x);
+			nxt.push_back(y);
+			nxt.push_back(z);
+			sort(nxt.begin(), nxt.end());
+			if (!visit[nxt[0]][nxt[1]][nxt[2]]) {
+				visit[nxt[0]][nxt[1]][nxt[2]] = 1;
+				q.push(nxt);
+			}
+		}
+
+	}
+	cout << 0;
+	return 0;
+	
+}
