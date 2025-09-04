5b012e8137b30ab2929a1510b6120dbe00738db6
김민주 <110026510+mint130@users.noreply.github.com>
2025-09-04 15:26:41 +0900

    [Gold II] Title: 친구 네트워크, Time: 348 ms, Memory: 34884 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+using namespace std;
+map<string, int> m1;
+map<int, string> m2;
+int t;
+int parent[200001];
+int child[200001];
+int findParent(int x) {
+	if (x == parent[x]) return x;
+	else return parent[x] = findParent(parent[x]);
+}
+int makeUnion(int a, int b) {
+	a = findParent(a);
+	b = findParent(b);
+	if (a != b)
+	{
+		if (child[a] <= child[b]) {
+			// 친구 더 큰쪽으로 합침
+			parent[a] = b;
+			child[b] += child[a];
+			return child[b];
+		}
+		else {
+			parent[b] = a;
+			child[a] += child[b];
+			return child[a];
+		}
+	}
+}
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> t;
+	for (int tc = 1; tc <= t; tc++) {
+		int n;
+		cin >> n;
+		m1.clear();
+		m2.clear(); // 초기화
+		for (int i = 1; i <= 200000; i++) {
+			parent[i] = i;
+			child[i] = 1;
+		}
+		int cnt = 0; // 실제 사람
+		for (int i = 0; i < n; i++) {
+			string s1, s2;
+			cin >> s1 >> s2;
+			if (m1[s1] == 0) {
+				cnt++;
+				// 번호 배정
+				m1[s1] = cnt;
+				m2[cnt] = s1;
+			}
+			if (m1[s2] == 0) {
+				cnt++;
+				m1[s2] = cnt;
+				m2[cnt] = s2;
+			}
+			int num1 = m1[s1];
+			int num2 = m1[s2];
+			if (findParent(num1) != findParent(num2)) {
+				// 합치고 자식도 합침
+				cout << makeUnion(num1, num2) << "\n";
+			}
+			else {
+				cout << child[findParent(num1)] << "\n";
+			}
+		}
+	}
+}
