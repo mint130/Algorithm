c3dbbd4de6556c4fc27b4b81b72083e1aaea1348
김민주 <110026510+mint130@users.noreply.github.com>
2025-09-10 02:41:14 +0900

    [Gold V] Title: 수 고르기, Time: 16 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,30 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 int n, m;
-
-int arr[100000];
-int main() {
-
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	sort(arr, arr + n);
-	int st = 0;
-	int en = 0;
-	int ans = 2000000001;
-	while (st < n && en < n) {
-		if (arr[en] - arr[st] >= m) {
-			ans = min(ans, arr[en] - arr[st]);
-			st++;
-		}
-		else {
-			en++;
-		}
-	}
-	cout << ans;
+long long arr[100001];
+long long ans = 2000000001;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    int st = 0;
+    int en = 0;
+    while (st <= en && en < n)
+    {
+        long long tmp = arr[en] - arr[st]; // 두 수의 차
+        if (tmp < m)
+        {
+            // 차이가 m보다 작으면 en 증가
+            en++;
+        }
+        else
+        {
+            // 차이가 m보다 크거나 같으면
+            if (tmp == m)
+            {
+                cout << m;
+                return 0;
+            }
+            ans = min(ans, tmp);
+            st++;
+        }
+    }
+    cout << ans;
 }
