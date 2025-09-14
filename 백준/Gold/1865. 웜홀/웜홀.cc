3cb91a982028b099106166694eb2b357a9260c99
김민주 <110026510+mint130@users.noreply.github.com>
2025-09-14 21:30:36 +0900

    [Gold III] Title: 웜홀, Time: 36 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int T, n, m, w;
+vector<pair<int, pair<int, int>>> v; //  비용, 시작, 끝
+int dist[501];
+bool bell_ford(int num)
+{
+    fill(dist, dist + n + 1, 987654321);
+    dist[num] = 0;
+    // n번 반복
+    for (int i = 1; i <= n; i++)
+    {
+        for (auto edge : v)
+        {
+            int cost = edge.first;
+            int st = edge.second.first;
+            int en = edge.second.second;
+            if (dist[en] > dist[st] + cost)
+            {
+                dist[en] = dist[st] + cost;
+                // if(i==n) return true;
+            }
+        }
+    }
+    // return false;
+    for (auto edge : v)
+    {
+        int cost = edge.first;
+        int st = edge.second.first;
+        int en = edge.second.second;
+        if (dist[en] > dist[st] + cost)
+        {
+            // 음수 사이클 존재
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++)
+    {
+        cin >> n >> m >> w;
+        // n 지점 수 m 도로 수 w 웜홀 수
+        v.clear();
+        for (int i = 0; i < m; i++)
+        {
+            int s, e, t;
+            // 번호 시간
+            cin >> s >> e >> t;
+            v.push_back({t, {s, e}});
+            v.push_back({t, {e, s}});
+        }
+        for (int i = 0; i < w; i++)
+        {
+            int s, e, t;
+            cin >> s >> e >> t;
+            v.push_back({-t, {s, e}});
+        }
+        // 모든 노드가 0과 연결되게
+        for (int i = 1; i <= n; i++)
+        {
+            v.push_back({0, {i, 0}});
+        }
+        if (bell_ford(0))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
