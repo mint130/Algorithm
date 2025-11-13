7c0fc9a09232077cb1d9f25bec6e94aedaa5aec9
김민주 <110026510+mint130@users.noreply.github.com>
2025-11-13 15:20:07 +0900

    [level 3] Title: 가장 먼 노드, Time: 8.69 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,38 +1,41 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <cstring>
-#include <queue>
-#include <string.h>
 #include <algorithm>
+#include <queue>
+#include <iostream>
 using namespace std;
-vector<int> adj[20002];
-int dist[20002];
+vector<int> v[20001];
+int dist[20001];
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
+    fill(dist, dist+n+1, -1);
+    dist[1]=0;
     for(int i=0;i<edge.size();i++){
-        int st=edge[i][0];
-        int en=edge[i][1];
-        adj[st].push_back(en);
-        adj[en].push_back(st); 
+        int a = edge[i][0];
+        int b = edge[i][1];
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-    memset(dist, -1, sizeof(dist));
- 
     queue<int> q;
     q.push(1);
-    dist[1]=0;
     while(!q.empty()){
-        int cur=q.front();
+        int cur = q.front();
         q.pop();
-        for(auto nxt:adj[cur]){
-            if(dist[nxt]!=-1) continue; //방문한 적 있음
-            q.push(nxt);
-            dist[nxt]=dist[cur]+1;
+        
+        for(auto nxt:v[cur]){
+            if(dist[nxt]==-1){
+                dist[nxt]=dist[cur]+1;
+                q.push(nxt);
+            }
         }
     }
-    int maxdist=*max_element(dist, dist+n+1);
-    for(auto i:dist){
-        if(i==maxdist) answer++;
+    int m = -1;
+    for(int i=1;i<=n;i++){
+        m=max(m, dist[i]);
     }
+    for(int i=1;i<=n;i++){
+        if(dist[i]==m) answer++;
+    }
+    
     return answer;
 }
