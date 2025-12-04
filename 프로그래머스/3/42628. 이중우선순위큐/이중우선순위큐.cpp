6e6c752feaa293ed5bc37c6808dd93bc0293b60d
김민주 <110026510+mint130@users.noreply.github.com>
2025-12-04 15:17:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 10.64 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,43 +1,34 @@
 #include <string>
-#include <algorithm>
-#include <set>
 #include <vector>
 #include <iostream>
+#include <set>
 using namespace std;
-multiset<int> ms;
+multiset <int> ms; // 오름차순
 vector<int> solution(vector<string> operations) {
     vector<int> answer(2);
     for(int i=0;i<operations.size();i++){
-        string s=operations[i];
-        if(s[0]=='I'){
-            string str="";
-            for(int j=2;j<s.length();j++){
-                str+=s[j];
+        char c = operations[i][0];
+        if(c=='D'){
+            if(operations[i][2]=='-'){
+                //최솟값 삭제
+                if(!ms.empty()) ms.erase(ms.begin());
             }
-            ms.insert(stoi(str));
+            else {
+                //최댓값 삭제
+                if(!ms.empty()) ms.erase(--ms.end());
+            }
         }
         else {
-            if(s[2]=='-'){
-                //최소값 삭제
-                if(!ms.empty()){
-                    ms.erase(ms.begin());
-                }
-            }
-            else{
-                //최대값 삭제
-                if(!ms.empty()){
-                    ms.erase(--ms.end());
-                }
-            }
+            string s = operations[i].substr(1);
+            int num = stoi(s);
+            ms.insert(num);
         }
     }
-    if(ms.empty()){
-        answer[0]=0;
-        answer[1]=0;
-    }
-    else{
+    if(ms.empty()) {answer[0]=0; answer[1]=0;}
+    else {
         answer[1]=*ms.begin();
         answer[0]=*(--ms.end());
     }
+    
     return answer;
 }
