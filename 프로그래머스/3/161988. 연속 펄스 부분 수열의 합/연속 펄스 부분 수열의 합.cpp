4c2f366b386802524edfaf6e0295e8214a47da60
김민주 <110026510+mint130@users.noreply.github.com>
2025-12-29 00:53:00 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 11.64 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -1,39 +1,35 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <cstring>
 #include <algorithm>
 using namespace std;
-long long prefix1[500002];
-long long prefix2[500002];
+long long sum1[500001];
+long long sum2[500001];
+int n;
 long long solution(vector<int> sequence) {
-    long long ans1 = 0;
-    long long ans2 = 0;
-    int n = sequence.size();
-    vector<int> seq1;
-    vector<int> seq2;
+    long long answer = 0;
+    n = sequence.size();
+    vector<int> v1;
+    vector<int> v2;
+    // 1, -1, 1, -1 .. 홀수는 -1
+    // -1, 1, -1, 1 .. 짝수는 -1
     for(int i=0;i<n;i++){
-        int num=sequence[i];
-        if(i%2==1){
-            //seq1에 *-1
-            seq1.push_back(num*-1);
-            seq2.push_back(num);
+        if(i%2==0){
+            // 짝수
+            v1.push_back(sequence[i]);
+            v2.push_back(sequence[i]*-1);
         }
         else {
-            //seq2에 *-1
-            seq2.push_back(num*-1);
-            seq1.push_back(num);
+            v1.push_back(sequence[i]*-1);
+            v2.push_back(sequence[i]);
         }
     }
-    prefix1[0]=seq1[0];
-    prefix2[0]=seq2[0];
+    sum1[0]=v1[0];
+    sum2[0]=v2[0];
     for(int i=1;i<n;i++){
-        prefix1[i]=max(prefix1[i-1]+(long long)seq1[i], (long long)seq1[i]);
-        prefix2[i]=max(prefix2[i-1]+(long long)seq2[i], (long long)seq2[i]);
+        // i까지 중 누적 제일 큰 것
+        sum1[i]=max(sum1[i-1]+(long long)v1[i], (long long)v1[i]);
+        sum2[i]=max(sum2[i-1]+(long long)v2[i], (long long)v2[i]);
     }
     
-    
-    ans1=*max_element(prefix1, prefix1+n);
-    ans2=*max_element(prefix2, prefix2+n);
-    return max(ans1, ans2);
+    return max(*max_element(sum1, sum1+n+1), *max_element(sum2, sum2+n+1));
 }
