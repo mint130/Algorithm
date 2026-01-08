4a073e820f1fc299ea6633638a6f047dcafc30f1
김민주 <110026510+mint130@users.noreply.github.com>
2026-01-08 14:19:38 +0900

    [level 3] Title: 양과 늑대, Time: 0.86 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,50 +1,52 @@
 #include <string>
 #include <vector>
-#include <set>
+#include <queue>
 #include <iostream>
+#include <algorithm>
 using namespace std;
+bool visited[18][18][18]; // 노드 늑대 양
+vector<int> v[18];
 int answer = 0;
-bool visited[18][18][18]; // i번 노드에 j개의 양 k개의 늑대로 방문한 적 있는지
-vector<int> v[18]; // v[p]
-void dfs(int cur, vector<int> &info, int sheep, int wolf) {
+void dfs(int cur, int sheep, int wolf, vector<int> &info) {
     answer = max(answer, sheep);
     for(auto nxt:v[cur]){
-        if(info[nxt]==0 && !visited[nxt][sheep+1][wolf]){
-            visited[nxt][sheep+1][wolf]=1;
-            // 빈 노드로 바꿈
+        // 갈 수 있는 노드 nxt
+        if(info[nxt]==1 && wolf+1<sheep && !visited[nxt][wolf+1][sheep])
+        {
+            // 늑대가 있고 현재 늑대 갯수 +1 < 양
+            visited[nxt][wolf+1][sheep]=1;
+            // 늑대 없어짐
             info[nxt]=-1;
-            dfs(nxt, info, sheep+1, wolf);
+            dfs(nxt, sheep, wolf+1, info);
             // 원복
-            info[nxt]=0;
-            visited[nxt][sheep+1][wolf]=0;
-        }
-        else if(info[nxt]==1 && !visited[nxt][sheep][wolf+1] && sheep>wolf+1){
-            visited[nxt][sheep][wolf+1]=1;
-            info[nxt]=-1;
-            dfs(nxt, info, sheep, wolf+1);
             info[nxt]=1;
-            visited[nxt][sheep][wolf+1]=0;
+            visited[nxt][wolf+1][sheep]=0;
         }
-        else if(info[nxt]==-1 && !visited[nxt][sheep][wolf]){
-            // 비었음 
-            visited[nxt][sheep][wolf]=1;
-            dfs(nxt, info, sheep, wolf);
-            visited[nxt][sheep][wolf]=0;
+        else if(info[nxt]==0 && !visited[nxt][wolf][sheep+1]){
+            // 양이 있는 경우
+            visited[nxt][wolf][sheep+1]=1;
+            info[nxt]=-1;
+            dfs(nxt, sheep+1, wolf, info);
+            info[nxt]=0;
+            visited[nxt][wolf][sheep+1]=0;
+        }
+        else if(info[nxt]==-1 && !visited[nxt][wolf][sheep]){
+            visited[nxt][wolf][sheep]=1;
+            dfs(nxt, sheep, wolf, info);
+            visited[nxt][wolf][sheep]=0;
         }
     }
-
+    
 }
 int solution(vector<int> info, vector<vector<int>> edges) {
-
     for(int i=0;i<edges.size();i++){
-        int p = edges[i][0];
-        int c = edges[i][1];
-        v[p].push_back(c);
-        v[c].push_back(p);
+        int a = edges[i][0];
+        int b = edges[i][1];
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-    // 양 얻음
-    visited[0][1][0]=1;
-    info[0]=-1;
-    dfs(0, info, 1, 0);
+    visited[0][0][1]=1; // 0번에는 항상 양이 있음
+    info[0]=-1; // 빈칸
+    dfs(0, 1, 0, info);
     return answer;
 }
