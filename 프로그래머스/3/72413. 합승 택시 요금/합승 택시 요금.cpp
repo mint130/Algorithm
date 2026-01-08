68c7e003d0bc468af4f1347b17fcfd46be966e9d
김민주 <110026510+mint130@users.noreply.github.com>
2026-01-08 15:28:23 +0900

    [level 3] Title: 합승 택시 요금, Time: 13.69 ms, Memory: 5.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,40 +1,48 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <iostream>
 #include <algorithm>
-#include <cstring>
+#include <iostream>
 using namespace std;
 int dist[201][201];
-#define MAX 987654321
+int answer = 987654321;
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    int answer = MAX;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            dist[i][j]=MAX;
+            dist[i][j]=987654321;
         }
     }
     for(int i=0;i<fares.size();i++){
-        int c = fares[i][0];
-        int d = fares[i][1];
-        int f = fares[i][2]; 
-        dist[c][d]=f;
-        dist[d][c]=f;
+        int a = fares[i][0];
+        int b = fares[i][1];
+        int c = fares[i][2];
+        dist[a][b]=c;
+        dist[b][a]=c;
     }
+    
     for(int k=1;k<=n;k++){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(i==j) dist[i][j]=0;
-                if(dist[i][k]==MAX || dist[k][j]==MAX) continue;
-                dist[i][j]=min(dist[i][k]+dist[k][j], dist[i][j]);
+                else{
+                    if(dist[i][j]>dist[i][k]+dist[k][j])
+                        dist[i][j]=dist[i][k]+dist[k][j];
+                }
+                
             }
         }
     }
-    answer = dist[s][a]+dist[s][b];
+    
+    int aa = dist[s][a]; // 직통
+    int bb = dist[s][b];
+    answer = min({aa+bb, answer});
     for(int i=1;i<=n;i++){
-        if(s==i) continue;
-        if(dist[s][i] ==MAX || dist[i][a]== MAX || dist[i][b]== MAX) continue;
-        answer =min(answer, dist[s][i]+dist[i][a]+dist[i][b]);
+        // i가 중간지점 
+        if(s!=i && dist[s][i]!=987654321) {
+            int d = dist[s][i];
+            int aaa = dist[i][a];
+            int bbb = dist[i][b];
+            answer = min({answer, d+aaa+bbb});
+        }
     }
     return answer;
 }
