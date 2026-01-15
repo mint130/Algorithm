a20f1c2cc4e82da0f78a1412c2e02d7ddd29b221
김민주 <110026510+mint130@users.noreply.github.com>
2026-01-15 15:36:24 +0900

    [level 2] Title: 할인 행사, Time: 114.91 ms, Memory: 8.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -2,31 +2,30 @@
 #include <vector>
 #include <map>
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
+map<string, int> m;
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
-    int n=want.size();
-    int idx=0;
-    vector<int> num(number.begin(), number.end());
-    while(idx+10<=discount.size()){
-        //idx부터 10개씩 want에 있는거 확인하고 want에 1이라도 남아 있으면 idx++, 원래대로 want 복구
-        for(int i=0;i<10;i++){
-            string s=discount[idx+i];
-            for(int j=0;j<n;j++){
-                if(want[j]==s){
-                    num[j]--;
-                }
+    int n =want.size();
+    for(int i=0;i<want.size();i++){
+        string s= want[i];
+        int num = number[i];
+        m[s]=num;
+    }
+    for(int i=0;i+9<discount.size();i++){
+        map<string, int> m2;
+        for(int j=0;j<10;j++){
+            string s = discount[i+j];
+            m2[s]++;
+        }
+        bool flag = 1;
+        for(auto i:m2){
+            if(i.second != m[i.first]){
+                flag = 0;
+                break;
             }
         }
-        bool flag=0;
-        for(int j=0;j<n;j++){
-            if(num[j]!=0) flag=1;
-            num[j]=number[j]; //복구
-        }
-        if(!flag) answer++;
-        idx++;
+        if(flag) answer++;
     }
     return answer;
 }
