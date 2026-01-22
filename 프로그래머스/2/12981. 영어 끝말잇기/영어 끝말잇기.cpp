e2f2adf01d22c6bbea949c653933c9f05a6d63dc
김민주 <110026510+mint130@users.noreply.github.com>
2026-01-22 14:20:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,27 +1,35 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <set>
 #include <map>
-
+#include <iostream>
+#include <set>
 using namespace std;
-    map<string, int> m;
+map<string, int> m;
 vector<int> solution(int n, vector<string> words) {
     vector<int> answer(2);
-    vector<string> ans[n];
-    ans[0].push_back(words[0]);
-    m[words[0]]++;
-    for(int i=1;i<words.size();i++){
-        ans[i%n].push_back(words[i]);
-        char c1=words[i-1].back();
-        char c2=words[i].front();
-        m[words[i]]++;
-       
-        if(c1!=c2 || m[words[i]]>1){
-            answer[0]=i%n+1;
-            answer[1]=ans[i%n].size();
-            break;
+    for(int i=0;i<words.size();i++){
+        // 순서
+        int idx = i%n;
+        int num = i/n;
+        string cur = words[i];
+        if(i==0){
+            m[cur]=1;
+            continue;
         }
+        
+        string pre = words[i-1];
+        int len = pre.length();
+        char c = pre[len-1];
+        if(cur[0]==c && m[cur]==0){
+            m[cur]=1;
+        }
+        else {
+            answer[0]=idx+1;
+            answer[1]=num+1;
+            return answer;
+        }
+        
     }
 
     return answer;
