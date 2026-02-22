dbb913ecb93baf7bef3d3ee5a16411afe05ef4c9
김민주 <110026510+mint130@users.noreply.github.com>
2026-02-22 23:00:42 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 8.17 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,9 +1,43 @@
 #include <string>
 #include <vector>
-
+#include <numeric>
+#include <algorithm>
+#include <iostream>
 using namespace std;
-
+vector<int> v;
 int solution(vector<int> queue1, vector<int> queue2) {
-    int answer = -2;
-    return answer;
+    int answer = 987654321;
+    int n = queue1.size();
+    long long sum = accumulate(queue1.begin(), queue1.end(), 0L); // 찾는 숫자
+    sum+=accumulate(queue2.begin(), queue2.end(), 0L);
+    sum/=2;
+    for(auto i:queue1){
+        v.push_back(i);
+    }
+    for(auto i:queue2){
+        v.push_back(i);
+    }
+    int len = n*2;
+    int st = 0;
+    int en = 0;
+    int mid = n-1; // 중간
+    long long ans = v[0];
+    int cnt = 0;
+    while(st<=en && en<len){
+        if(ans<=sum){
+            if(ans==sum){
+                if(en>=mid){
+                    cnt = en - mid + st;
+                    answer = min(answer, cnt);
+                }
+            }
+            en++;
+            ans+=(long long)v[en];
+        }
+        else {
+            ans-=(long long)v[st];
+            st++;
+        }
+    }
+    return answer==987654321?-1:answer;
 }
