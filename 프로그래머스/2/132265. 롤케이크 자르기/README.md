3cb99381755f1cad03d597a1d0dece0fd3642e15
김민주 <110026510+mint130@users.noreply.github.com>
2026-03-08 21:02:19 +0900

    [level 2] Title: 롤케이크 자르기, Time: 206.83 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 37.5 MB, 시간: 205.66 ms
+메모리: 37.5 MB, 시간: 206.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 12일 03:06:33
+2026년 03월 08일 21:02:15
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,25 +1,19 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <map>
 using namespace std;
-map<int, int> a; //철수 토핑 가짓수
-map<int, int> b; //동생 토핑 가짓수
-
+map<int, int> m1; 
+map<int, int> m2;
 int solution(vector<int> topping) {
     int answer = 0;
-    
     for(int i=0;i<topping.size();i++){
-        //i번째 토핑 동생이 가짐
-        b[topping[i]]++;
+        m1[topping[i]]++; // m1에 전체 토핑 종류 저장
     }
     for(int i=0;i<topping.size();i++){
-        //i번째 토핑을 동생에게서 빼고 철수에게 넣음
-        a[topping[i]]++;
-        b[topping[i]]--;
-        if(b[topping[i]]==0) b.erase(topping[i]); 
-        //토핑 갯수가 작아져서 가짓수에서 빠지면 map에서 지워줌(size 함수 쓰기 위해)
-        if(a.size()==b.size()) answer++;
+        m1[topping[i]]--;
+        m2[topping[i]]++;
+        if(m1[topping[i]]==0) m1.erase(topping[i]);
+        if(m1.size()==m2.size()) answer++;
     }
     return answer;
 }
